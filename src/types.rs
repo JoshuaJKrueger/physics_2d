9a766affb410819225b239b652d7fc023fc0cbcd
JoshuaJKrueger <jkrew74@gmail.com>
2023-11-27 21:49:27 -0700

    Adds type aliases

---
+++ src/types.rs
@@ -0,0 +1,14 @@
+use ordered_float::OrderedFloat;
+
+pub type Meter = OrderedFloat<f64>;
+pub type MeterPerSec = f64;
+pub type NormalizedCoefficient = OrderedFloat<f64>;
+pub type Kilogram = f64;
+pub type InvKilogram = f64;
+pub type MeterSquaredPerKilogram = f64;
+pub type KilogramPerCubicMeter = f64;
+pub type KilogramMeterSquared = f64;
+pub type Radian = f64;
+pub type RadianPerSec = f64;
+pub type NewtonMeter = f64;
+pub type SquareMeter = OrderedFloat<f64>;
