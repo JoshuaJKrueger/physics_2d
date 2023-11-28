8eda9a3197c7c4afca34e9259b3138fc52bb8e45
JoshuaJKrueger <jkrew74@gmail.com>
2023-11-27 21:52:38 -0700

    Template for new collision system

--- src/collision.rs
+++ src/collision.rs
@@ -1,87 +1,54 @@
-use nalgebra::{Point2, Vector2};
+use nalgebra::Vector2;
 
-pub enum Shape {
-    Circle { radius: f64 },
-    Rectangle { width: f64, height: f64 },
+use crate::manifold::Manifold;
+use crate::object::{Object, Shape};
+
+fn circle_circle(manifold: &mut Manifold, a: &Object, b: &Object) {
+    match (&a.shape, &b.shape) {
+        (Shape::Circle { radius: rad_a }, Shape::Circle { radius: rad_b }) => {
+            let norm = b.tx.pos - a.tx.pos;
+            let dist_sqr = norm.norm_squared();
+            let rad = *rad_a + *rad_b;
+        
+            if dist_sqr >= *rad * *rad {
+                manifold.contact_count = 0;
+                return;
+            }
+        
+            manifold.contact_count = 1;
+            
+            let dist = dist_sqr.sqrt();
+            if dist == 0.0 {
+                manifold.penetration = *rad_a;
+                manifold.normal = Vector2::new(1.0, 0.0);
+                manifold.contacts[0] = a.tx.pos.coords;
+            } else {
+                manifold.penetration = rad - dist;
+                manifold.normal = norm / dist;
+                manifold.contacts[0] = manifold.normal * **rad_b + a.tx.pos.coords;
+            }
+        },
+        _ => unreachable!()
+    }
 }
 
-struct AABB
-{
-    min: Point2<f64>,
-    max: Point2<f64>,
+fn circle_polygon(manifold: &Manifold, a: &Object, b: &Object) {
+    match (&a.shape, &b.shape) {
+        (Shape::Circle { radius: rad }, Shape::Polygon { vertices: verts }) => {
+            unimplemented!();
+        },
+        (Shape::Polygon { vertices: verts }, Shape::Circle { radius: rad }) => {
+            circle_polygon(manifold, b, a)
+        },
+        _ => unreachable!()
+    }
 }
 
-struct Circle
-{
-    center: Point2<f64>,
-    radius: f64,
+fn polygon_polygon(manifold: &Manifold, a: &Object, b: &Object) {
+    match (&a.shape, &b.shape) {
+        (Shape::Polygon { vertices: verts_a }, Shape::Polygon { vertices: verts_b }) => {
+            unimplemented!();
+        },
+        _ => unreachable!()
+    }
 }
-
-pub struct Object
-{
-    pub position: Point2<f64>,
-    pub velocity: Vector2<f64>,
-    pub angle: f64,
-    pub shape: Shape,
-    pub mass: f64,
-    pub restitution: f64,
-    pub inv_mass: f64,
-}
-
-struct Manifold<'a>
-{
-    a: &'a Object,
-    b: &'a Object,
-    penetration: Option<f64>,
-    normal: Option<Vector2<f64>>,
-}
-
-fn aabb_aabb(a: &AABB, b: &AABB) -> bool {
-    // Check if the two Axis-Aligned Bounding Boxes (AABB) intersect along any axis.
-    // If there is separation along any axis, return false; otherwise, return true.
-    !(a.max.x < b.min.x || a.min.x > b.max.x || a.max.y < b.min.y || a.min.y > b.max.y)
-}
-
-fn circle_circle(a: &Circle, b: &Circle) -> bool {
-    // TODO: Compare performance
-    // let r = a.radius + b.radius;
-    // r *= r;
-    // r < (a.x + b.x)^2 + (a.y + b.y)^2
-    (b.center - a.center).norm_squared() < (a.radius + b.radius).powi(2)
-}
-
-// // TODO: Refactor this function
-// fn ResolveCollision(a: &Object, b: &Object) {
-//     let rel_vel = b.velocity - a.velocity;
-//     // TODO: Get normal
-//     let vel_norm = rel_vel.dot(&normal);
-
-//     // Make sure objects are moving towards one another
-//     if (vel_norm < 0) {
-//         let e = min(a.restitution, b.restitution);
-//         let j = -(1 + e) * vel_norm;
-
-//         j /= a.inv_mass + b.inv_mass;
-
-//         let impulse = j * normal;
-
-//         a.velocity -= a.inv_mass * impulse;
-//         b.velocity += b.inv_mass * impulse;
-
-//         // TODO: Might be simpler, check if it functions the same
-//         // let mass_sum = a.mass + b.mass;
-//         // let ratio = a.mass / mass_sum;
-//         // a.velocity -= ratio * impulse;
-//         // ratio = b.mass / mass_sum;
-//         // b.velocity += ratio * impulse;
-//     }
-// }
-
-// // TODO: Refactor
-// fn correct_float_error(a: &Object, b: &Object) {
-//     const PERCENT: f64 = 0.2;
-//     const JITTER_THESHOLD: f64 = 0.01;
-//     let correction = max(penetration_depth - k_slop, 0.0) / (a.inv_mass + b.inv_mass) * PERCENT * n;
-//     a.position -= a.inv_mass * correction;
-//     b.position += b.inv_mass * correction;
-// }
