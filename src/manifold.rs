4fea9f07c42e691bd6d336b00c43f2bbb559a43e
JoshuaJKrueger <jkrew74@gmail.com>
2023-11-29 21:07:28 -0700

    Adds constructor

--- src/manifold.rs
+++ src/manifold.rs
@@ -5,6 +5,7 @@ use ordered_float::OrderedFloat;
 
 use crate::object::Object;
 use crate::types::{Meter, NormalizedCoefficient};
+use crate::constants::GRAVITY;
 
 pub struct Manifold<'a> {
     a: &'a mut Object,
@@ -24,11 +25,25 @@ fn cross(a: f64, b: Vector2<f64>) -> Vector2<f64> {
 }
 
 impl<'a> Manifold<'a> {
-    fn solve() {
+    pub fn new(a: &'a mut Object, b: &'a mut Object) -> Manifold<'a> {
+        Manifold {
+            a,
+            b,
+            penetration: OrderedFloat(0.0),
+            normal: Vector2::zeros(),
+            contacts: [Vector2::zeros(); 2],
+            contact_count: 0,
+            mixed_restitution: OrderedFloat(0.0),
+            mixed_dynamic_friction: OrderedFloat(0.0),
+            mixed_static_friction: OrderedFloat(0.0),
+        }
+    }
+
+    pub fn solve(&mut self) {
         unimplemented!()
     }
 
-    fn initialize(&mut self) {
+    pub fn initialize(&mut self, dt: f64) {
         self.mixed_restitution = min(self.a.mat.restitution, self.b.mat.restitution);
         self.mixed_dynamic_friction = OrderedFloat((self.a.mat.dynamic_friction * self.b.mat.dynamic_friction).sqrt());
         self.mixed_static_friction = OrderedFloat((self.a.mat.static_friction * self.b.mat.static_friction).sqrt());
@@ -38,13 +53,13 @@ impl<'a> Manifold<'a> {
             let b_radii = self.contacts[i] - self.b.tx.pos.coords;
             let rel_vel = self.b.kinematics.vel + cross(self.b.kinematics.angular_vel, b_radii) - self.a.kinematics.vel - cross(self.a.kinematics.angular_vel, a_radii);
 
-            // if rel_vel.norm_squared() < (dt * gravity).norm_squared() + EPSILON {
-            //     self.mixed_restitution = OrderedFloat(0.0);
-            // }
+            if rel_vel.norm_squared() < (dt * GRAVITY).norm_squared() + EPSILON {
+                self.mixed_restitution = OrderedFloat(0.0);
+            }
         }
     }
 
-    fn apply_impulse(&mut self) {
+    pub fn apply_impulse(&mut self) {
         if self.a.mass_data.mass == INFINITY && self.b.mass_data.mass == INFINITY {
             self.infinite_mass_correction();
             return;
@@ -55,7 +70,7 @@ impl<'a> Manifold<'a> {
         }
     }
 
-    fn positional_correction(&mut self) {
+    pub fn positional_correction(&mut self) {
         const PEN_ALLOWANCE: NormalizedCoefficient = OrderedFloat(0.05);
         const PERCENT_CORRECTION: NormalizedCoefficient = OrderedFloat(0.4);
         let correction = *(max(self.penetration - PEN_ALLOWANCE, OrderedFloat(0.0)) / (self.a.mass_data.inv_mass + self.b.mass_data.inv_mass)) * self.normal * *PERCENT_CORRECTION;
