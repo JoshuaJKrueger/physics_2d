fa91c365715e07e54c22b2529628f36a0c5d3805
JoshuaJKrueger <jkrew74@gmail.com>
2023-11-29 18:24:09 -0700

    Adds test objects and calls render and update

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,9 @@ use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
 
+use nalgebra::{Point2, Vector2};
+use ordered_float::OrderedFloat;
+
 mod collision;
 mod manifold;
 mod object;
@@ -16,6 +19,7 @@ mod scene;
 mod types;
 
 use scene::Scene;
+use object::{Object, Shape, Transform, Material, MassData, Kinematics};
 
 pub struct App<'a> {
     gl: GlGraphics, // OpenGL drawing backend.
@@ -27,15 +31,62 @@ impl<'a> App<'a> {
         use graphics::*;
 
         const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
-        const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
 
         self.gl.draw(args.viewport(), |c: Context, gl: &mut GlGraphics| {
             clear(BLACK, gl);
+
+            self.scene.render(c, gl);
         });
     }
 
     fn update(&mut self, args: &UpdateArgs) {
+        self.scene.step(args.dt);
     }
+}
+
+fn create_test_objects() -> Vec<Object> {
+    // Create a circle
+    let circle = Object::new(
+        Shape::Circle { radius: OrderedFloat(50.0) },
+        Transform::new(Point2::new(100.0, 100.0)),
+        Some(Material::new(1.0, OrderedFloat(0.5), OrderedFloat(0.3), OrderedFloat(0.2))),
+        Some(MassData::new(5.0, 1000.0)),
+        Some(Kinematics::new(Vector2::new(0.0, 0.0), 0.0, 0.0)),
+    );
+
+    // Create a convex polygon (triangle)
+    let convex_polygon = Object::new(
+        Shape::Polygon {
+            vertices: vec![
+                Point2::new(200.0, 200.0),
+                Point2::new(250.0, 300.0),
+                Point2::new(300.0, 200.0),
+            ],
+        },
+        Transform::new(Point2::new(0.0, 0.0)), // Set the position accordingly
+        Some(Material::new(1.0, OrderedFloat(0.5), OrderedFloat(0.3), OrderedFloat(0.2))),
+        Some(MassData::new(5.0, 1000.0)),
+        Some(Kinematics::new(Vector2::new(0.0, 0.0), 0.0, 0.0)),
+    );
+
+    // Create a concave polygon (hourglass shape)
+    let concave_polygon = Object::new(
+        Shape::Polygon {
+            vertices: vec![
+                Point2::new(350.0, 200.0),
+                Point2::new(350.0, 300.0),
+                Point2::new(500.0, 400.0),
+                Point2::new(400.0, 300.0),
+                Point2::new(500.0, 200.0),
+            ],
+        },
+        Transform::new(Point2::new(0.0, 0.0)), // Set the position accordingly
+        Some(Material::new(1.0, OrderedFloat(0.5), OrderedFloat(0.3), OrderedFloat(0.2))),
+        Some(MassData::new(5.0, 1000.0)),
+        Some(Kinematics::new(Vector2::new(0.0, 0.0), 0.0, 0.0)),
+    );
+
+    vec![circle, convex_polygon, concave_polygon]
 }
 
 fn main() {
@@ -52,7 +103,7 @@ fn main() {
     // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
-        scene: Scene { objects: Vec::new(), contacts: Vec::new() }
+        scene: Scene { objects: create_test_objects(), contacts: Vec::new() }
     };
 
     let mut events = Events::new(EventSettings::new());
