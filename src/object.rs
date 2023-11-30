f3e5503ed5562150c1ec5320d31d881758f21dc1
JoshuaJKrueger <jkrew74@gmail.com>
2023-11-29 18:24:43 -0700

    Adds constructors and draw method

--- src/object.rs
+++ src/object.rs
@@ -1,5 +1,8 @@
 use nalgebra::{Point2, Rotation2, Vector2};
 use ordered_float::OrderedFloat;
+use graphics::{Context, ellipse, line::Line, Transformed};
+use opengl_graphics::GlGraphics;
+use rand::Rng;
 
 use crate::types::{Meter, SquareMeter, Radian, Kilogram, InvKilogram, KilogramMeterSquared, MeterSquaredPerKilogram, KilogramPerCubicMeter, NormalizedCoefficient, RadianPerSec, NewtonMeter};
 
@@ -32,11 +35,36 @@ pub struct Transform {
     orientation: Radian,
 }
 
+impl Transform {
+    pub fn new(pos: Point2<f64>) -> Self {
+        Transform {
+            pos,
+            rot: Rotation2::identity(),
+            scale: Vector2::new(1.0, 1.0),
+            orientation: 0.0,
+        }
+    }
+}
+
 pub struct MassData {
     pub mass: Kilogram,
     pub inv_mass: InvKilogram,
     moment_inertia: KilogramMeterSquared,
     inv_m_inertia: MeterSquaredPerKilogram,
+}
+
+impl MassData {
+    pub fn new(mass: Kilogram, moment_inertia: KilogramMeterSquared) -> Self {
+        let inv_mass = if mass != 0.0 { 1.0 / mass } else { 0.0 };
+        let inv_m_inertia = if moment_inertia != 0.0 { 1.0 / moment_inertia } else { 0.0 };
+
+        MassData {
+            mass,
+            inv_mass,
+            moment_inertia,
+            inv_m_inertia,
+        }
+    }
 }
 
 pub struct Material {
@@ -46,10 +74,31 @@ pub struct Material {
     pub static_friction: NormalizedCoefficient,
 }
 
+impl Material {
+    pub fn new(density: KilogramPerCubicMeter, restitution: NormalizedCoefficient, dynamic_friction: NormalizedCoefficient, static_friction: NormalizedCoefficient) -> Self {
+        Material {
+            density,
+            restitution,
+            dynamic_friction,
+            static_friction,
+        }
+    }
+}
+
 pub struct Kinematics {
     pub vel: Vector2<f64>,
     pub angular_vel: RadianPerSec,
     torque: NewtonMeter,
+}
+
+impl Kinematics {
+    pub fn new(vel: Vector2<f64>, angular_vel: RadianPerSec, torque: NewtonMeter) -> Self {
+        Kinematics {
+            vel,
+            angular_vel,
+            torque,
+        }
+    }
 }
 
 pub struct Object
@@ -63,6 +112,40 @@ pub struct Object
 }
 
 impl Object {
+    pub fn new(shape: Shape, tx: Transform, mat: Option<Material>, mass_data: Option<MassData>, kinematics: Option<Kinematics>) -> Self {
+        let mat = mat.unwrap_or_else(|| {
+            // Generate random material properties
+            let density = rand::thread_rng().gen_range(0.1..10.0);
+            let restitution = rand::thread_rng().gen_range(0.0..1.0);
+            let dynamic_friction = rand::thread_rng().gen_range(0.0..1.0);
+            let static_friction = rand::thread_rng().gen_range(0.0..1.0);
+
+            Material::new(density, OrderedFloat(restitution), OrderedFloat(dynamic_friction), OrderedFloat(static_friction),
+            )
+        });
+
+        let mass_data = mass_data.unwrap_or_else(|| {
+            // Generate random mass data
+            let mass = rand::thread_rng().gen_range(1.0..10.0);
+            let moment_inertia = rand::thread_rng().gen_range(0.1..10.0);
+
+            MassData::new(mass, moment_inertia)
+        });
+
+        let kinematics = kinematics.unwrap_or_else(|| {
+            // Generate random kinematics properties
+            let vel = Vector2::new(rand::thread_rng().gen_range(-10.0..10.0), rand::thread_rng().gen_range(-10.0..10.0));
+            let angular_vel = rand::thread_rng().gen_range(-1.0..1.0);
+            let torque = rand::thread_rng().gen_range(-1.0..1.0);
+
+            Kinematics::new(vel, angular_vel, torque)
+        });
+
+        let force = Vector2::new(0.0, 0.0); // Initialize force to zero
+
+        Object {shape, tx, mat, mass_data, kinematics, force}
+    }
+
     fn apply_force(&mut self, f: Vector2<f64>) {
         self.force += f;
     }
@@ -70,5 +153,24 @@ impl Object {
     fn apply_impulse(&mut self, imp: Vector2<f64>, contact_vec: Vector2<f64>) {
         self.kinematics.vel += self.mass_data.inv_mass * imp;
         self.kinematics.angular_vel += self.mass_data.inv_m_inertia * cross(&contact_vec, &imp);
+    }
+
+    pub fn draw(&self, c: Context, gl: &mut GlGraphics) {
+        const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+        match &self.shape {
+            Shape::Circle { radius } => {
+                ellipse::Ellipse::new_border(WHITE, 1.0).draw(ellipse::circle(self.tx.pos.x, self.tx.pos.y, **radius), &c.draw_state, c.transform, gl);
+            }
+            Shape::Polygon { vertices } => {
+                // piston polygon only has a filled version
+                for i in 0..vertices.len() {
+                    let next_index = (i + 1) % vertices.len();
+                    let start = [vertices[i].x, vertices[i].y];
+                    let end = [vertices[next_index].x, vertices[next_index].y];
+    
+                    Line::new(WHITE, 1.0).draw([start[0], start[1], end[0], end[1]], &c.draw_state, c.transform.trans(self.tx.pos.x, self.tx.pos.y), gl);
+                }
+            }
+        }
     }
 }
