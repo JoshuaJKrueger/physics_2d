a9948f311bb85757130720b48314f98ebabe1a27
JoshuaJKrueger <jkrew74@gmail.com>
2023-11-30 23:13:35 -0700

    Consolidates loose consts in program

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,13 @@
 use nalgebra::Vector2;
+use ordered_float::OrderedFloat;
 
-use crate::types::MeterPerSquaredSecond;
+use crate::types::{MeterPerSquaredSecond, NormalizedCoefficient};
 
-pub const GRAVITY: Vector2<MeterPerSquaredSecond> = Vector2::new(0.0, -9.8);
+pub const GRAVITY: Vector2<MeterPerSquaredSecond> = Vector2::new(0.0, 0.0);
+pub const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+pub const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+pub const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+pub const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+// For positional correction
+pub const PEN_ALLOWANCE: NormalizedCoefficient = OrderedFloat(0.05);
+pub const PERCENT_CORRECTION: NormalizedCoefficient = OrderedFloat(0.4);
